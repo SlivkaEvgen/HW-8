abfc1ea296b001d8aeb2d79ded6d32f073434d2f
SlivkaEvgen <79520449+SlivkaEvgen@users.noreply.github.com>
2021-04-28 10:48:01 +0300

    Delete MyList1.java

--- MyList1.java
+++
@@ -1,9 +0,0 @@
-//package MyArrayList;
-//
-//public interface MyList1<T> {
-//    void add(T element); //добавляет элемент в конец
-//    void remove(int index); //удаляет элемент под индексом
-//    void clear();           //очищает коллекцию
-//    int size();             //возвращает размер коллекции
-//    T get(int index);       //возвращает элемент под индексом
-//}
